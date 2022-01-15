f68c8dd027344d2d2cb51c2ace4c895de73eaff8
Diego <dglpferreira@gmail.com>
2022-01-15 11:39:58 -0300

    feat: Added Input Component
      Component will be used for the CreateTransactionPage
      Has a label and a textinput

---
+++ src/pages/CreateTransaction/Input.js
@@ -0,0 +1,30 @@
+import { StyleSheet, View, Text, TextInput } from "react-native";
+import { colors, sizes } from "../../styles";
+
+export default function Input({ state, setState, label }) {
+  return (
+    <View style={local.container}>
+      <Text style={local.text}>{label}</Text>
+      <TextInput value={state} onChangeText={setState} style={local.input} />
+    </View>
+  );
+}
+
+const local = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  text: {
+    paddingHorizontal: sizes.l,
+    fontSize: sizes.l,
+    color: colors.foreground,
+  },
+  input: {
+    alignSelf: "stretch",
+    padding: sizes.s,
+    margin: sizes.l,
+    color: colors.foreground,
+    borderColor: colors.foreground,
+    borderWidth: 1,
+  },
+});
