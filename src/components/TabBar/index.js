9066a5e5bc85837e87044c41426b4b5c4c34c98b
Diego <dglpferreira@gmail.com>
2022-01-15 16:08:44 -0300

    refactor: Updated navigation names

--- src/components/TabBar/index.js
+++ src/components/TabBar/index.js
@@ -35,7 +35,7 @@ export default function TabBar({ state, descriptors, navigation }) {
       {firstHalf.map(returnTabBarIcon)}
       <TabBarPlus
         callback={() => {
-          navigation.navigate({ name: "CreateTransaction" });
+          navigation.navigate({ name: "CreateTransaction.SelectWhat" });
         }}
       />
       {secondHalf.map(returnTabBarIcon)}
