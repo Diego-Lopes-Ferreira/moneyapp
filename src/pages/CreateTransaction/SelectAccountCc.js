c0015bac6c4e630dd9ba341a3d83af22a6b4ba1a
Diego <dglpferreira@gmail.com>
2022-01-15 16:08:21 -0300

    feat: Page 2 of the CreateTransaction flow
      This page shows the cards and navigates to
      the ohter pages

---
+++ src/pages/CreateTransaction/SelectAccountCc.js
@@ -0,0 +1,64 @@
+import { ScrollView, View, Text, StyleSheet } from "react-native";
+import global, { colors, sizes } from "../../styles";
+import ButtonIconName from "../../components/Button/IconName";
+import HeaderCancelSave from "./HeaderCancelSave";
+
+export default function SelectAccountCc({ navigation }) {
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
+  function handleCc(id) {
+    navigation.navigate("CreateTransaction.CcTransactionForm", {
+      ccid: "12345678",
+    });
+  }
+
+  function handleAccount(id) {
+    navigation.navigate("CreateTransaction.AccountTransactionForm", {
+      account_id: "12345678",
+    });
+  }
+
+  return (
+    <View style={global.container}>
+      <HeaderCancelSave
+        leftPress={handleGoBack}
+        title="Selecione a conta ou o cartão"
+      />
+      <ScrollView style={local.scrollView}>
+        <Text style={global.h3}>Contas</Text>
+        <ButtonIconName
+          label="Banco X"
+          callback={() => handleAccount(1)}
+          iconName="home"
+          iconColor={colors.background}
+          iconBgColor={colors.foreground}
+        />
+        <ButtonIconName
+          label="Banco Y"
+          callback={() => handleAccount(2)}
+          iconName="home"
+          iconColor={colors.background}
+          iconBgColor={colors.foreground}
+        />
+
+        <Text style={global.h3}>Cartões</Text>
+        <ButtonIconName
+          label="Cartão Teste"
+          callback={() => handleCc(1)}
+          iconName="home"
+          iconColor={colors.background}
+          iconBgColor={colors.foreground}
+        />
+      </ScrollView>
+    </View>
+  );
+}
+
+const local = StyleSheet.create({
+  scrollView: {
+    width: "100%",
+    paddingHorizontal: sizes.s,
+  },
+});
