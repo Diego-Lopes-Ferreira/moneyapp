20d44552acbb2c5f27f4b787b240243f38f76e9f
Diego <dglpferreira@gmail.com>
2022-01-15 16:07:07 -0300

    feat: Added button with icon

---
+++ src/components/Button/IconName.js
@@ -0,0 +1,48 @@
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import global, { colors, sizes } from "../../styles";
+
+export default function ButtonIconName({
+  label,
+  callback,
+  iconName,
+  iconColor,
+  iconBgColor,
+}) {
+  return (
+    <TouchableOpacity activeOpacity={0.7} onPress={callback} style={local.btn}>
+      <Feather
+        style={[local.icon, { backgroundColor: iconBgColor }]}
+        name={iconName}
+        size={sizes.s}
+        color={iconColor}
+      />
+      <Text style={local.txt}>{label}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const local = StyleSheet.create({
+  btn: {
+    alignSelf: "stretch",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: colors.backgroundLight,
+    borderRadius: sizes.xxxl,
+    paddingVertical: sizes.s,
+    paddingHorizontal: sizes.xxs,
+    margin: sizes.s,
+  },
+  txt: {
+    flex: 1,
+    ...global.text_normal,
+    color: colors.foreground,
+  },
+  icon: {
+    padding: sizes.s,
+    marginRight: sizes.s,
+    marginLeft: 0,
+    borderRadius: sizes.xxxl,
+  },
+});
