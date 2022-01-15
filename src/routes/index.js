87a77b559be5f4107370b07b3d910ab1931fea43
Diego <dglpferreira@gmail.com>
2022-01-15 16:11:24 -0300

    refactor: Updated rout names

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,8 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 // Pages
 import TabNavigator from "./tabs";
-import CreateTransaction from "../pages/CreateTransaction";
+import CreateTransactionCreateWhat from "../pages/CreateTransaction/SelectWhat";
+import SelectAccountCc from "../pages/CreateTransaction/SelectAccountCc";
 
 const Stack = createNativeStackNavigator();
 
@@ -10,7 +11,14 @@ export default function StackRoutes() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="TabNavigator" component={TabNavigator} />
-      <Stack.Screen name="CreateTransaction" component={CreateTransaction} />
+      <Stack.Screen
+        name="CreateTransaction.SelectWhat"
+        component={CreateTransactionCreateWhat}
+      />
+      <Stack.Screen
+        name="CreateTransaction.SelectAccountCc"
+        component={SelectAccountCc}
+      />
     </Stack.Navigator>
   );
 }
