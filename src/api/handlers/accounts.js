6aef2078059532e4897484459fc47fa038492017
Diego <dglpferreira@gmail.com>
2022-01-17 10:21:06 -0300

    feat: insert + refactor: list
      feat: Added insert function
      refactor: Used runDb instead of doing it manually

--- src/api/handlers/accounts.js
+++ src/api/handlers/accounts.js
@@ -1,3 +1,18 @@
-export function read() {
-  return "wasd";
+import { runDb } from "../db";
+
+export async function list() {
+  const query = "SELECT * FROM accounts";
+  return await runDb(query, []);
+}
+
+export async function insert(newAccount) {
+  let { name, icon, color, description, balance } = newAccount;
+  console.log(parseInt(balance));
+  // let id = Math.random().toString(36).slice(2, 10);
+  let query = `
+    INSERT INTO accounts
+    (name, icon, color, description, balance)
+    VALUES
+    (?, ?, ?, ?, ?)`;
+  runDb(query, [name, icon, color, description, balance]);
 }
