fec3a8f7acec96555aeafcd2fca8ceb4ac66d97b
Diego <dglpferreira@gmail.com>
2022-01-17 10:20:25 -0300

    feat: Added runDb
      Async function that runs a query and returns the rows

--- src/api/db/index.js
+++ src/api/db/index.js
@@ -2,4 +2,20 @@ import { openDatabase } from "expo-sqlite";
 
 const db = openDatabase("db.db");
 
+export async function runDb(query, list) {
+  return new Promise((resolve) => {
+    db.transaction(
+      (tx) => {
+        tx.executeSql(query, list, (_, { rows }) => {
+          resolve(rows);
+        });
+      },
+      (err) => {
+        console.log(err);
+        resolve(false);
+      }
+    );
+  });
+}
+
 export default db;
