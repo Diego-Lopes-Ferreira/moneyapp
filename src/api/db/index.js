a2425c8795aec308f26524b79c8cfe4b259ec4ed
Diego <dglpferreira@gmail.com>
2022-01-17 15:44:51 -0300

    feat: Added empty list handler
      Now runDb can be called without the list

--- src/api/db/index.js
+++ src/api/db/index.js
@@ -3,10 +3,11 @@ import { openDatabase } from "expo-sqlite";
 const db = openDatabase("db.db");
 
 export async function runDb(query, list) {
+  let theList = list || [];
   return new Promise((resolve) => {
     db.transaction(
       (tx) => {
-        tx.executeSql(query, list, (_, { rows }) => {
+        tx.executeSql(query, theList, (_, { rows }) => {
           resolve(rows);
         });
       },
