5de5ca0ba4171c998a1bd885f1a6dde6123c563e
Diego <dglpferreira@gmail.com>
2022-01-20 21:30:19 -0300

    feat: Exported new functions for the app

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,13 @@
-import * as accountHandler from "./handlers/accounts";
-import * as ccHandler from "./handlers/credit_cards";
 import * as accountTransactionHandler from "./handlers/account_transactions";
+import * as accountHandler from "./handlers/accounts";
+import * as categoriesHandler from "./handlers/categories";
+import * as ccTransactionHandler from "./handlers/cc_transactions";
+import * as ccHandler from "./handlers/credit_cards";
+import * as transferencesHandler from "./handlers/transferences";
 
-export const account = { ...accountHandler };
-export const credit_cards = { ...ccHandler };
 export const account_transaction = { ...accountTransactionHandler };
+export const account = { ...accountHandler };
+export const categories = { ...categoriesHandler };
+export const cc_transaction = { ...ccTransactionHandler };
+export const credit_cards = { ...ccHandler };
+export const transferences = { ...transferencesHandler };
