b3ba7535b110caa2a12989d76820a72501c92ce9
Diego <dglpferreira@gmail.com>
2022-01-21 13:36:49 -0300

    feat: Config Categories Pages

---
+++ src/ConfigCategories/CategoryForm.js
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import global, { colors } from "../styles";
+
+import { View, Text } from "react-native";
+import PickBetweenTwoTypes from "../components/PickBetweenTwoTypes";
+import Input from "../components/Input";
+import ButtonSimple from "../components/Button/Simple";
+
+export default function CreateNewCategory({ route, navigation }) {
+  const { category } = route.params;
+
+  const [type, set_type] = useState("");
+  const [name, set_name] = useState("");
+  const [icon, set_icon] = useState("");
+  const [color, set_color] = useState("");
+
+  useEffect(() => {
+    if (category) {
+      set_type(category.type);
+      set_name(category.name);
+      set_icon(category.icon);
+      set_color(category.color);
+    }
+  }, []);
+
+  function handleSave() {
+    if (category) {
+      // update with the current values
+    } else {
+      // create a new category
+    }
+  }
+
+  return (
+    <View style={global.container}>
+      <Text style={global.h1}>Criar Nova Categoria</Text>
+      <PickBetweenTwoTypes
+        type={type}
+        setType={set_type}
+        leftLabel="Saida"
+        leftType="expense"
+        leftColor={colors.foreground}
+        rightType="income"
+        rightLabel="Entrada"
+        rightColor={colors.foreground}
+      />
+      <Input state={name} setState={set_name} label="Nome" />
+      <Input state={icon} setState={set_icon} label="Icone" />
+      <Input state={color} setState={set_color} label="Cor" />
+      <ButtonSimple label="Salvar" callback={handleSave} />
+    </View>
+  );
+}
---
+++ src/ConfigCategories/index.js
@@ -0,0 +1,22 @@
+import { StyleSheet, ScrollView, View, Text } from "react-native";
+import global from "../../styles";
+import ButtonSimple from "../components/Button/Simple";
+
+export default function ConfigCategoriesPage({ route, navigation }) {
+  return (
+    <View style={global.container}>
+      <Text style={local.txt}>Categorias</Text>
+      <ButtonSimple
+        label="Criar Nova Categoria"
+        callback={() => navigation.navigate("ConfigCategories.Create")}
+      />
+      <Text style={local.txt}>Lista...</Text>
+    </View>
+  );
+}
+
+const local = StyleSheet.create({
+  txt: {
+    ...global.h1,
+  },
+});
