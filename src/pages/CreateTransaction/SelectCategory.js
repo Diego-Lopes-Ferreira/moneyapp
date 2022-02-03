8bdd2e3480611371e65e3781150feb5292d3d6ec
Diego <dglpferreira@gmail.com>
2022-02-02 21:43:59 -0300

    feat: Create account transaction + Go back home

--- src/pages/CreateTransaction/SelectCategory.js
+++ src/pages/CreateTransaction/SelectCategory.js
@@ -1,9 +1,17 @@
+import { useEffect, useState } from "react";
 import { StyleSheet, ScrollView, View, Text } from "react-native";
-import ButtonIconName from "../../components/Button/IconName";
+import { useIsFocused, StackActions } from "@react-navigation/native";
+
 import global, { sizes, colors } from "../../styles";
+
+import ButtonIconName from "../../components/Button/IconName";
 import HeaderCancelSave from "./HeaderCancelSave";
 
+import { categories, account_transaction } from "../../api";
+
 export default function SelectCategory({ route, navigation }) {
+  const isFocused = useIsFocused();
+  const [cats, set_cats] = useState();
   const { data } = route.params;
 
   function handleGoBack() {
@@ -14,7 +22,28 @@ export default function SelectCategory({ route, navigation }) {
     data.category = id;
     console.log(data);
     // TODO: save "data" on database
+    account_transaction.create({
+      account_id: data.account_id,
+      type: data.type,
+      value: data.value,
+      name: data.name,
+      desc: data.desc,
+      date: data.date,
+      category: id,
+    });
+    navigation.reset({
+      index: 0,
+      routes: [{ name: "TabNavigator" }],
+    });
   }
+
+  async function fetchCategories() {
+    let { _array } = await categories.list();
+    set_cats(_array);
+  }
+  useEffect(() => {
+    fetchCategories();
+  }, [isFocused]);
 
   return (
     <View style={global.container}>
@@ -23,6 +52,20 @@ export default function SelectCategory({ route, navigation }) {
         title="Selecione a categoria"
       />
       <ScrollView style={local.scrollView}>
+        {cats?.length > 0 ? (
+          cats.map((cat, index) => (
+            <ButtonIconName
+              key={index}
+              label={cat.name}
+              callback={() => handleSelectCategory(cat.id)}
+              iconName={cat.icon}
+              iconColor={colors.background}
+              iconBgColor={cat.color}
+            />
+          ))
+        ) : (
+          <Text style={global.text_normal}>Nenhuma Categoria</Text>
+        )}
         <ButtonIconName
           label="Banco X"
           callback={() => handleSelectCategory(123)}
